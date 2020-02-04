0ca1932ae0fb103a73e9c80af41d0e60982af03d
VimukthiMayadunne <vimukthimayadunne@gmail.com>
2020-02-04 11:38:44 +0530

    code formating

--- src/getVersion.ts
+++ src/getVersion.ts
@@ -1,24 +1,23 @@
-async function getVersionNumber(version:number){
-    return new Promise(async function(resolve,reject){
+async function getVersionNumber(version: number) {
+  return new Promise(async function(resolve, reject) {
     var apiVersion;
-    try{
-        switch (version) {
-            case (2.7):
-                apiVersion='v0.14';
-                break;
-            case (3.0):
-                apiVersion='v0.15';
-                break;
-            case (3.1):
-                apiVersion='v0.16';
-                break;
-        }
-        resolve (apiVersion)
+    try {
+      switch (version) {
+        case 2.7:
+          apiVersion = "v0.14";
+          break;
+        case 3.0:
+          apiVersion = "v0.15";
+          break;
+        case 3.1:
+          apiVersion = "v0.16";
+          break;
+      }
+      resolve(apiVersion);
+    } catch (err) {
+      console.error(err);
+      reject(err);
     }
-    catch(err){
-        console.error(err)
-        reject(err)
-    }
-    })
+  });
 }
-module.exports = {getVersionNumber}
+module.exports = { getVersionNumber };
--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,44 @@
-import {Command, flags} from '@oclif/command'
-
+import { Command, flags } from "@oclif/command";
 
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
-const apiToken = require('./apiToken');
-const getVersion = require('./getVersion');
-
-
+const apiToken = require("./apiToken");
+const getVersion = require("./getVersion");
 
 class Apimtoken extends Command {
-  static description = 'Single line CLI to get an accses token from Wso2-API_M'
+  static description = "Single line CLI to get an accses token from Wso2-API_M";
 
   static flags = {
-    // add --version flag to show CLI version
-    version: flags.version({char: 'v'}),
-    help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    scopes: flags.string({char: 's', description: 'Scopes required to be genarated'}),
-    uri: flags.string ({char: 'u', description: 'URL for the wso2 API-M'}),
-    username: flags.string ({char: 'n', description: 'User Name'}),
-    password: flags.string ({char: 'p', description: 'Password'}),
-    apimversion: flags.string({char: 'a', description: 'API-M Version'})
-  }
-  static args = [{name: 'file'}]
+    version: flags.version({ char: "v" }),
+    help: flags.help({ char: "h" }),
+    scopes: flags.string({
+      char: "s",
+      description: "Scopes required to be genarated"
+    }),
+    uri: flags.string({ char: "u", description: "URL for the wso2 API-M" }),
+    username: flags.string({ char: "n", description: "User Name" }),
+    password: flags.string({ char: "p", description: "Password" }),
+    apimversion: flags.string({ char: "a", description: "API-M Version" })
+  };
+  static args = [{ name: "file" }];
 
   async run() {
-    const {args, flags} = this.parse(Apimtoken)
-    const scopes = flags.scopes
-    const uri = flags.uri || 'https://localhost:9443'
-    const userName = flags.username 
-    const password = flags.password 
-    const apimV = flags.apimversion || '3.1'
-    const apimVersion=await getVersion.getVersionNumber(parseFloat(apimV));
-    var rslt=await apiToken.getCredintials(uri,userName,password,scopes,'v0.16');
-    var key = 'Bearer '+await rslt
-    console.log(key)
+    const { args, flags } = this.parse(Apimtoken);
+    const scopes = flags.scopes;
+    const uri = flags.uri || "https://localhost:9443";
+    const userName = flags.username;
+    const password = flags.password;
+    const apimV = flags.apimversion || "3.1";
+    const apimVersion = await getVersion.getVersionNumber(parseFloat(apimV));
+    var rslt = await apiToken.getCredintials(
+      uri,
+      userName,
+      password,
+      scopes,
+      apimVersion
+    );
+    var key = "Bearer " + (await rslt);
+    console.log(key);
   }
 }
 
-export = Apimtoken
+export = Apimtoken;
